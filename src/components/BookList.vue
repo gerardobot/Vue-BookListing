25311fe5e657fe2cd1ed3a4bb48af30081760e85
Gerardo Gómez Puerto <ggomez@atsistemas.com>
2018-06-15 14:59:57 +0200

    Module 2 half done

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-  </div>
 
+    <ul>
+      <li v-for="book in books">{{book.title}}, {{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
     export default {
         name: 'BookList.vue',
-        data: () => {return {title: "All Books"}}
+        data: () => {return {
+          title: "All Books",
+          books: [
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+          ]
+        }}
     }
 </script>
 
 <style scoped>
   h1, h2 {
     font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: block;
+    margin: 0 10px;
   }
 </style>

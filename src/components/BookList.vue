f79b7c5fd380f337f6750edb3e54f923465ec3c7
Gerardo Gómez Puerto <ggomez@atsistemas.com>
2018-06-15 14:34:03 +0200

    Module 1

---
+++ src/components/BookList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'BookList.vue',
+        data: () => {return {title: "All Books"}}
+    }
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
